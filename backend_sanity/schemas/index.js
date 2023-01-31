085fbbdede6cecd003a83532412f86ea36082c42
Rahmonjon22 <rahmonjon2@yahoo.com>
2023-01-31 21:22:27 +0100

    schemas

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,5 +1,14 @@
 import testimonials from "./testimonials";
+import brands from './brands';
+import abouts from './abouts';
+import experiences from './experiences';
+import skills from './skills';
+import workExperience from './workExperience';
+import contact from './contact';
+import works from './works'
+
 
 export const schemaTypes = [
-    testimonials
+    works, testimonials, brands, abouts, skills, workExperience, experiences, contact
+    /* Your types here! */
 ]
